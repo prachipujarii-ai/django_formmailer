cc8f0d648bb72e45bbdeb61950910c6dfca513b4
Prachi Pujari <pspujarii.912@gmail.com>
2026-06-06 13:11:57 +0530

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,11 @@
+#Model for the application form
+from django import forms
+
+
+class ApplicationForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    date = forms.DateField()
+    occupation = forms.CharField(max_length=80)
+
